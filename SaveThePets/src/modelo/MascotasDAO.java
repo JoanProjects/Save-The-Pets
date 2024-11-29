19d11d1e6bdd82c5c21ae6fd16692db2f09ef3af
JoanProjects <189782774+JoanProjects@users.noreply.github.com>
2024-11-29 15:32:30 -0400

    Agregar al DAO metodo para listar todos los registros de mascotas

--- SaveThePets/src/modelo/MascotasDAO.java
+++ SaveThePets/src/modelo/MascotasDAO.java
@@ -14,12 +14,14 @@ public class MascotasDAO {
     PreparedStatement ps;
     ResultSet         rs;
 
-    //Listar registro de mascotas
+    //Listar registro de mascotas a buscar
+    //@param valorBuscar
+    //@return datos
     public List listarRegistro(String valorBuscar){
         
-        String sql = "select nombre, sexo, pesoKg, especie_id, raza_id,"+
-                     " vacunacion_estado, adopcion_estado, colaborador_id from mascotas"+
-                     " where id||nombre||sexo||pesoKG||vacunacion_estado like '%"+valorBuscar+"%'" +
+        String sql = "SELECT id, nombre, sexo, pesoKg, especie_id, raza_id,"+
+                     " vacunacion_estado, adopcion_estado, colaborador_id FROM mascotas"+
+                     " WHERE id||nombre||sexo||pesoKG||vacunacion_estado like '%"+valorBuscar+"%'" +
                      " OR especie_id =(SELECT id FROM especies WHERE nombre = '"+valorBuscar+"')" + 
                      " OR raza_id =(SELECT id FROM razas WHERE nombre = '"+valorBuscar+"')" +
                      " OR colaborador_id =(SELECT id FROM colaboradores WHERE nombre = '"+valorBuscar+"')";
@@ -50,4 +52,38 @@ public class MascotasDAO {
         return datos;
     }
     
+    //Metodo lista todos los registros en Mascotas
+    //@return datos
+    public List listar(){
+        String sql = "select mascotas.id, mascotas.nombre, mascotas.sexo," +
+                     " mascotas.pesoKG, especies.nombre, razas.nombre," +
+                     " vacunacion_estado, adopcion_estado, colaboradores.nombre" +
+                    " FROM mascotas JOIN especies ON mascotas.especie_id = especies.id" + 
+                    " JOIN razas ON mascotas.raza_id = razas.id" +
+                    " JOIN colaboradores ON mascotas.colaborador_id = colaboradores.id";
+        List<Mascotas>datos = new ArrayList<>();
+        try{
+            
+            con = conectar.conectar();
+            ps  = con.prepareStatement(sql);
+            rs  = ps.executeQuery();
+            
+            while(rs.next()){
+                Mascotas c = new Mascotas();
+                c.setId(rs.getInt(1));
+                c.setNombre(rs.getString(2));
+                c.setSexo(rs.getString(3));
+                c.setPesoKG(rs.getString(4));
+                c.setEspecie(rs.getString(5));
+                c.setRaza(rs.getString(6));
+                c.setVacunacionEstado(rs.getString(7));
+                c.setAdopcionEstado(rs.getString(8));
+                c.setColaborador(rs.getString(9));
+                datos.add(c);
+            }
+        }catch(SQLException ex){
+            System.out.println("ERROR no se pudo listar Mascotas: " + ex);
+        }
+        return datos;
+    }
 }
