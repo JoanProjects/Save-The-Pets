6846580919d3c12c25f969fa77bcb0d2e8d5df94
WilfredoFC <180332250+WilfredoFC@users.noreply.github.com>
2024-12-01 12:39:23 -0400

    Desarrollar logica del DTO para Login

--- SaveThePets/src/modelo/Login.java
+++ SaveThePets/src/modelo/Login.java
@@ -6,4 +6,58 @@ package modelo;
  */
 public class Login {
 
+    int id;
+    String usuario;
+    String clave;
+    String nombre;
+    String apellido;
+
+    public Login() {
+    }
+
+    public Login(int id, String usuario, String clave) {
+        this.id = id;
+        this.usuario = usuario;
+        this.clave = clave;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 }
